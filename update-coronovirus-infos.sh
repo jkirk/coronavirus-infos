e99ad90b36e23879520ed5c42667ce23c897ab0b
Darshaka Pathirana <dpat@syn-net.org>
2020-03-17 19:02:45 +0100

    update-coronovirus-infos.sh: Fix typo in commit message

--- update-coronovirus-infos.sh
+++ update-coronovirus-infos.sh
@@ -12,8 +12,8 @@ cd "${SCRIPT_PATH}"
 wget -q https://www.sozialministerium.at/Informationen-zum-Coronavirus/Coronavirus---Haeufig-gestellte-Fragen.html -O Coronavirus---Haeufig-gestellte-Fragen.html
 STAND="Stand: $(sed -n -e "0,/(Stand:.*)/ s/.*(Stand: \(.*\))/\1/p" Coronavirus---Haeufig-gestellte-Fragen.html)"
 git add Coronavirus---Haeufig-gestellte-Fragen.html
-git status | grep -q 'nothing to commit' || git commit --untracked-files=no -m "Coronvirus FAQ ${STAND}"
+git status | grep -q 'nothing to commit' || git commit --untracked-files=no -m "Coronavirus FAQ ${STAND}"
 wget -q "https://www.sozialministerium.at/Informationen-zum-Coronavirus/Neuartiges-Coronavirus-(2019-nCov).html" -O Neuartiges-Coronavirus-\(2019-nCov\).html
 STAND="Stand: $(sed -n -e "0,/Stand,/ s/.*Stand, \(.*\)Bisher.*/\1/p" Neuartiges-Coronavirus-\(2019-nCov\).html)"
 git add Neuartiges-Coronavirus-\(2019-nCov\).html
-git status | grep -q 'nothing to commit' || git commit --untracked-files=no -m "Coronvirus ${STAND}"
+git status | grep -q 'nothing to commit' || git commit --untracked-files=no -m "Coronavirus ${STAND}"
