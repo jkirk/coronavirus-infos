613babade63e156974ae48e81d77e26c1e0962f0
Darshaka Pathirana <dpat@syn-net.org>
2020-03-17 10:50:12 +0100

    update-coronovirus-infos.sh: Option --short implies --dry-run

--- update-coronovirus-infos.sh
+++ update-coronovirus-infos.sh
@@ -12,8 +12,8 @@ cd "${SCRIPT_PATH}"
 wget -q https://www.sozialministerium.at/Informationen-zum-Coronavirus/Coronavirus---Haeufig-gestellte-Fragen.html -O Coronavirus---Haeufig-gestellte-Fragen.html
 STAND="Stand: $(sed -n -e "0,/(Stand:.*)/ s/.*(Stand: \(.*\))/\1/p" Coronavirus---Haeufig-gestellte-Fragen.html)"
 git add Coronavirus---Haeufig-gestellte-Fragen.html
-git commit --short -uno -m "Coronvirus FAQ ${STAND}"
+git commit --untracked-files=no -m "Coronvirus FAQ ${STAND}"
 wget -q "https://www.sozialministerium.at/Informationen-zum-Coronavirus/Neuartiges-Coronavirus-(2019-nCov).html" -O Neuartiges-Coronavirus-\(2019-nCov\).html
 STAND="Stand: $(sed -n -e "0,/Stand,/ s/.*Stand, \(.*\)Bisher.*/\1/p" Neuartiges-Coronavirus-\(2019-nCov\).html)"
 git add Neuartiges-Coronavirus-\(2019-nCov\).html
-git commit --short -uno -m "Coronvirus ${STAND}"
+git commit --untracked-files=no -m "Coronvirus ${STAND}"
