644c02a1e399b8db079a1223364753b45bcb9da2
Darshaka Pathirana <dpat@syn-net.org>
2020-03-18 17:50:23 +0100

    update-coronovirus-infos.sh: Make HREF absolute
    To allow viewing the html files directly the links need to be absolute.
    This can be achieved by adding <base href="https://www.sozialministerium.at/"> after <head>.
    
    Closes: jkirk/coronavirus-infos#1
    Thanks: Thrauka Pathirana @360path for the bug report and <base> hint

--- update-coronovirus-infos.sh
+++ update-coronovirus-infos.sh
@@ -10,10 +10,12 @@
 SCRIPT_PATH="$(dirname $(realpath $0))"
 cd "${SCRIPT_PATH}"
 wget -q https://www.sozialministerium.at/Informationen-zum-Coronavirus/Coronavirus---Haeufig-gestellte-Fragen.html -O Coronavirus---Haeufig-gestellte-Fragen.html
+sed -i '/<head>/a\\t<base href="https://www.sozialministerium.at/">' Coronavirus---Haeufig-gestellte-Fragen.html
 STAND="Stand: $(sed -n -e "0,/(Stand:.*)/ s/.*(Stand: \(.*\))/\1/p" Coronavirus---Haeufig-gestellte-Fragen.html)"
 git add Coronavirus---Haeufig-gestellte-Fragen.html
 git status | grep -q 'nothing to commit' || git commit --untracked-files=no -m "Coronavirus FAQ ${STAND}"
 wget -q "https://www.sozialministerium.at/Informationen-zum-Coronavirus/Neuartiges-Coronavirus-(2019-nCov).html" -O Neuartiges-Coronavirus-\(2019-nCov\).html
+sed -i '/<head>/a\\t<base href="https://www.sozialministerium.at/">' Neuartiges-Coronavirus-\(2019-nCov\).html
 STAND="Stand: $(sed -n -e "0,/Stand,/ s/.*Stand, \(.*\)Bisher.*/\1/p" Neuartiges-Coronavirus-\(2019-nCov\).html)"
 git add Neuartiges-Coronavirus-\(2019-nCov\).html
 git status | grep -q 'nothing to commit' || git commit --untracked-files=no -m "Coronavirus ${STAND}"
